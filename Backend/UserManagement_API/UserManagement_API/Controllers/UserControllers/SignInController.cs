3e5e319f021ab211e793652400e8810e85a6c2b5
Janindumaleesha <janindu.maleesha@app360.lk>
2025-02-12 13:48:25 +0530

    Creasted login process

---
+++ Backend/UserManagement_API/UserManagement_API/Controllers/UserControllers/SignInController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using UserManagement_API.Config;
+using UserManagement_API.Interfaces.UserInterfaces;
+using UserManagement_API.Models.UserModels;
+using UserManagement_API.Models;
+
+namespace UserManagement_API.Controllers.UserControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SignInController : ControllerBase
+    {
+        private readonly ISignInService signInService;
+
+        public SignInController(ISignInService _signInService)
+        {
+            signInService = _signInService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ResponseResult<ModelSignInResponse>>> SignIn([FromBody, Required] ModelSignInRequest model)
+        {
+            if (Request.Headers.TryGetValue("AccessToken", out var _token))
+            {
+                if (_token != ConfigManager.JwtKey)
+                {
+                    return Unauthorized(new ResponseResult<ModelSignInResponse>("Unauthorized API Access."));
+                }
+
+                if (string.IsNullOrWhiteSpace(model.UserName) || string.IsNullOrWhiteSpace(model.Password))
+                { return new ResponseResult<ModelSignInResponse>(ErrorMessageCollection.MissingRequired); }
+
+                var requestIP = HttpContext.Connection.RemoteIpAddress?.ToString();
+
+                UserData _user = new() { UserName = model.UserName };
+
+                return await signInService.ManageSignIn(_user, model);
+            }
+
+            return BadRequest(new ResponseResult<ModelSignInResponse>("API Key Not Attached."));
+        }
+    }
+}
--- Backend/UserManagement_API/UserManagement_API/Models/UserModels/ModelSignInResponse.cs
+++ Backend/UserManagement_API/UserManagement_API/Models/UserModels/ModelSignInResponse.cs
@@ -3,7 +3,6 @@
     public class ModelSignInResponse
     {
         public ModelJWTResponse JWTResponse { get; set; }
-        public bool IsFirstTimeSignIn { get; set; }
         public ModelUserDetails UserDetails { get; set; }
     }
 }
