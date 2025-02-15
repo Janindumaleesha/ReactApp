084ac317e6512a64998057fc8de4a94f6069cc2d
Janindumaleesha <janindu.maleesha@app360.lk>
2025-02-15 18:07:28 +0530

    Fix bugs

--- Backend/UserManagement_API/UserManagement_API/Models/ConfigManager.cs
+++ Backend/UserManagement_API/UserManagement_API/Models/ConfigManager.cs
@@ -1,10 +1,29 @@
-﻿namespace UserManagement_API.Models
+﻿using System.Data.SqlClient;
+
+namespace UserManagement_API.Models
 {
     public class ConfigManager
     {
         public static string Environment { get; set; }
-        public static string ConnectionString { get; set; }
+        public static DBConnectionDetails DBConnectionDetails { get; set; }
         public static string JwtKey { get; set; }
         public static string BaseUrl { get; set; }
+
+        public static string ConnectionString => SetConnectionString(DBConnectionDetails);
+
+        private static string SetConnectionString(DBConnectionDetails _model)
+        {
+            SqlConnectionStringBuilder builder = new()
+            {
+                ConnectionString = _model.ConnectionString
+            };
+
+            return builder.ConnectionString;
+        }
+    }
+
+    public class DBConnectionDetails
+    {
+        public string ConnectionString { get; set; }
     }
 }
