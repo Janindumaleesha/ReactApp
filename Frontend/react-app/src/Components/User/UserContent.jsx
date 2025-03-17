056392bb1e42bf541c7b6f2a1b5075f35144b35d
Janindumaleesha <janindu.maleesha@app360.lk>
2025-03-17 23:43:56 +0530

    Update table data

--- Frontend/react-app/src/Components/User/UserContent.jsx
+++ Frontend/react-app/src/Components/User/UserContent.jsx
@@ -51,10 +51,6 @@ const rows = [
   createData('Mexico', 'MX', 126577691, 1972550),
   createData('Japan', 'JP', 126317000, 377973),
   createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
 ];
 
 function UserContent() {
